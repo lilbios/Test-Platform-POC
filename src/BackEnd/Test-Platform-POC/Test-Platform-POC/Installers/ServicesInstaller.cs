987c44082451f2a13de4540d57f6010ca1166fca
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2021-02-01 01:43:44 +0200

    Added ServiceInstaller class

---
+++ src/BackEnd/Test-Platform-POC/Test-Platform-POC/Installers/ServicesInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Test_Platform_POC.Data;
+
+namespace Test_Platform_POC.Installers
+{
+    public class ServicesInstaller : IInstaller
+    {
+        public void AddService(IServiceCollection service, IConfiguration configuration = null)
+        {
+            service.AddSingleton<AppDbContext>();
+        }
+    }
+}
