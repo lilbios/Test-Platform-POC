5d43a49cb49437274c7991e9438b818fba3cfe15
v-yshamrai <v-yshamrai@microsoft.com>
2021-02-12 22:47:26 +0200

    Removed useless directories and fixed declaration of methods in IBaseGenericRepository

--- src/BackEnd/Test-Platform-POC/Test-Platform-POC/Data/Interfaces/IBaseGenericRepository.cs
+++ src/BackEnd/Test-Platform-POC/Test-Platform-POC/Data/Interfaces/IBaseGenericRepository.cs
@@ -10,14 +10,18 @@ namespace Test_Platform_POC.Data.Interfaces
 {
     public interface IBaseGenericRepository<TEntity> where TEntity : class
     {
-        Task<IMongoCollection<TEntity>> GetCollectionAsync(Expression<Func<TEntity, bool>> filter = null);
-        Task<IMongoCollection<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null);
+        Task<IMongoCollection<TEntity>> GetCollectionAsync(Expression<Func<TEntity, bool>> filter = null,
+            Expression<Func<IMongoQueryable<TEntity>, IOrderedMongoQueryable<TEntity>>> orderBy = null);
+        Task<IMongoCollection<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, 
+            Expression<Func<IMongoQueryable<TEntity>, IOrderedMongoQueryable<TEntity>>> orderBy = null);
         Task<TEntity> AddAsync(TEntity record);
         Task<TEntity> AddRecordsAsync(IEnumerable<TEntity> records);
         Task<TEntity> UpdateRecordsAsync(IEnumerable<TEntity> records);
         Task<TEntity> UpdateAsync(Guid id, TEntity record);
         Task RemoveAsync(Expression<Func<TEntity, bool>> filter = null);
         Task RemoveRecordsAsync(Expression<Func<TEntity, bool>> filter = null);
+        Task<IMongoCollection<TResponse>> GetWithGroupByAsync<TResponse>(Expression<Func<TEntity,object>> groupBy = null,
+            Expression<Func<TEntity, bool>> filter = null);
 
     }
 }
